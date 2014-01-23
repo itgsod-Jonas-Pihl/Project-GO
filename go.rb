2ce538afd459cad2865771d2c5f8930deaecb438
itgsod-Jonas-Pihl <jonas.pihl@elev.it-gymnasiet.se>
2014-01-23 16:19:42 +0100

    deleted some unnecessary code

--- go.rb
+++ go.rb
@@ -2,14 +2,7 @@ class GamersOnline < Sinatra::Base
   Faye::WebSocket.load_adapter('thin')
   use Faye::RackAdapter, :mount => '/faye', :timeout => 45
 
-
-
   get '/' do
     slim :index
   end
-
-  get '/room' do
-    slim :room, :locals => { :room => params[:room], :username => params[:username] }
-  end
-
 end
