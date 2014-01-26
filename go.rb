25c67b41d69c1d60a57b2268488eb98399f74587
itgsod-Jonas-Pihl <jonas.pihl@elev.it-gymnasiet.se>
2014-01-26 16:23:14 +0100

    Added steam openid login

--- go.rb
+++ go.rb
@@ -1,4 +1,16 @@
+require "openid/store/filesystem"
+require "omniauth/strategies/steam"
+
 class GamersOnline < Sinatra::Base
+
+ api_key = "8DA685124F4E525EC2A97F2C932F3763"
+
+  use OmniAuth::Builder do
+    provider :steam, api_key, :storage => OpenID::Store::Filesystem.new("/tmp")
+  end
+
+  enable :sessions
+
   Faye::WebSocket.load_adapter('thin')
   use Faye::RackAdapter, :mount => '/faye', :timeout => 45
 
@@ -16,6 +28,11 @@ class GamersOnline < Sinatra::Base
 
   get '/lobby' do
     slim :'./lobby/lobby'
+  end
+
+  post '/auth/steam/callback' do
+    content_type "text/plain"
+    request.env["omniauth.auth"].info.to_hash.inspect
   end
 
 end
