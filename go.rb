560d72fca8557caf2b7411b1446d1bb7b5a56d0d
itgsod-Jonas-Pihl <jonas.pihl@elev.it-gymnasiet.se>
2014-02-06 12:27:58 +0100

    Added Bootstrap

--- go.rb
+++ go.rb
@@ -4,20 +4,24 @@ require 'net/http'
 
 
 class GamersOnline < Sinatra::Base
-
- api_key = "8DA685124F4E525EC2A97F2C932F3763"
-
+  api_key = "8DA685124F4E525EC2A97F2C932F3763"
   use OmniAuth::Builder do
     provider :steam, api_key, :storage => OpenID::Store::Filesystem.new("/tmp")
   end
-
-  enable :sessions
-
   Faye::WebSocket.load_adapter('thin')
   use Faye::RackAdapter, :mount => '/faye', :timeout => 45
 
+  enable :sessions
+
   get '/' do
     slim :frontpage
+  end
+
+  post '/auth/steam/callback' do
+    content_type "text/plain"
+    @openid = request.env["omniauth.auth"].extra.raw_info.to_hash
+    session[:steamid] = @openid['steamid'].to_i
+    redirect '/'
   end
 
   get '/browse' do
@@ -29,7 +33,6 @@ class GamersOnline < Sinatra::Base
   end
 
   get '/lobby' do
-
     unless session[:steamid] == nil
       uri = URI("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=#{api_key}&steamids=#{session[:steamid]}")
       @user = JSON.parse(Net::HTTP.get(uri))
@@ -43,17 +46,14 @@ class GamersOnline < Sinatra::Base
     end
   end
 
-  post '/auth/steam/callback' do
-    content_type "text/plain"
-    @openid = request.env["omniauth.auth"].extra.raw_info.to_hash
-    session[:steamid] = @openid['steamid']
-
-  end
-  get '/test' do
-    if session[:steamid].to_i == 76561198021297355
-      "wohooo"
+  get '/profile/:id' do |id|
+    if session[:steamid] == id.to_i
+      uri = URI("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=#{api_key}&steamid=#{id.to_i}&include_appinfo=")
+      @games = JSON.parse(Net::HTTP.get(uri))
+      @games.inspect
+      slim :'/profile/profile'
     else
-      "buuuuh"
+      redirect '/'
     end
   end
 end
